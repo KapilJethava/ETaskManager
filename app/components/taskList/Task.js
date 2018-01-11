6eed2aa3244de8077803e9b2456f584d88370a38
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-01-11 18:09:03 +0530

    Task list related stuff maintained and modified

---
+++ app/components/taskList/Task.js
@@ -0,0 +1,93 @@
+import React, { Component } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { commonStyles, Icon, ActionButton, styleConstant } from './../../commonModules';
+
+export class Task extends React.Component {
+	constructor(props){
+		super(props);
+	}
+	render() {
+		return (
+			<View style={styles.contentWrapper}>
+				<View>
+					<Icon name='home' style={styles.catIcon}/>
+				</View>
+				<View style={styles.taskData}>
+					<View style={styles.row1}>
+						<Text numberOfLines={1} style={styles.title}>{this.props.task.title}</Text>
+						<Text style={styles.time}>{this.props.task.time}</Text>
+					</View>
+					<View style={styles.row2}>
+					<Text numberOfLines={1} style={styles.desc}>{this.props.task.description}</Text>
+						<View style={[styles.priorityContainer, { backgroundColor:this.props.task.priority.color }]}>
+							<Text style={styles.priority}>{' '}</Text>
+						</View>
+					</View>
+				</View>
+			</View>
+		);
+	}
+}
+const styles = StyleSheet.create({
+	contentWrapper:{
+		flex:1,
+		flexDirection: 'row',
+		backgroundColor: '#FFF',
+		padding: 7
+	},
+	catIcon:{
+		fontSize:30,
+		borderWidth:1,
+		borderColor:'#ddd',
+		padding:6,
+		borderRadius:2,
+		color: styleConstant.themeColor,
+		width:44,
+		height:44
+	},
+	taskData:{
+		flex:1,
+		flexDirection:'column',
+		marginLeft:10
+	},
+	row1:{
+		flex:1,
+		flexDirection: 'row',
+	},
+	row2:{
+		flex: 1,
+		flexDirection: 'row',
+		marginTop:2
+	},
+	title:{
+		flex:1,
+		fontSize:14,
+		color: '#000',
+		fontWeight:'bold',
+		alignSelf:'flex-end'
+	},
+	time: {
+		alignSelf: 'flex-end',
+		fontSize: 13,
+		color:styleConstant.themeColor
+	},
+	desc: {
+		flex: 1,
+		fontSize:12,
+		alignSelf:'flex-start'
+	},
+	priorityContainer:{
+		alignSelf: 'flex-start',
+		padding: 3,
+		borderRadius: 10,
+		width:10,
+		height:10,
+	},
+	priority:{
+		fontSize: 11,
+		color: '#fff',
+		flex:1,
+		alignSelf:'center'
+	}
+});
+
--- app/components/taskList/TaskList.js
+++ app/components/taskList/TaskList.js
@@ -1,18 +1,61 @@
 import React, { Component } from 'react';
-import { Text } from 'react-native';
+import { ScrollView, Text, FlatList } from 'react-native';
 import { TabDetailWrapper } from './../tabDetailWrapper/TabDetailWrapper';
 import { commonStyles, Icon, ActionButton, styleConstant } from './../../commonModules';
+import { Task } from './Task';
 
 export class TaskList extends React.Component {
 	render() {
 		const { navigate } = this.props.navigation;
 		return (
 			<TabDetailWrapper>
-				<Text style={{ backgroundColor: 'transparent' }}>Task List</Text>
+				<ScrollView>
+					<FlatList
+						data={mockData.taskList}
+						renderItem={({ item }) => <Task task={item} />}
+					/>
+				</ScrollView>
+				{/* <Text style={{ backgroundColor: 'transparent' }}>Task List</Text> */}
 				<ActionButton buttonColor={styleConstant.addTaskButtonColor}
-					icon={<Icon name={styleConstant.addTaskIconName} style={commonStyles.actionButtonIcon} />}/>
+					icon={<Icon name={styleConstant.addTaskIconName} style={commonStyles.actionButtonIcon} />} />
 			</TabDetailWrapper>
 		);
 	}
 }
+class TaskModel {
+	constructor(key, title, desc, time, priority) {
+		this.key = key;
+		this.title = title;
+		this.description = desc;
+		this.time = time;
+		this.priorityKey = priority;
+	}
+	get priority(){
+		return priorityEnum[this.priorityKey];
+	}
+	set priority(value){
+		this.priorityKey = value;
+	}
+}
+const priorityEnum = {
+	0: { color: 'grey', identifier: 'None' },
+	1: { color:'red', identifier:'High'},
+	2: { color: 'blue', identifier: 'Medium' },
+	3: { color: 'green', identifier: 'Low' }
+};
+const mockData = {
+	taskList: [
+		new TaskModel(1, 'Pick your son from school', 'Pick your son from school','10:00 AM',1),
+		new TaskModel(2, 'Email details to client', 'Email details to client', '10:15 AM', 2),
+		new TaskModel(3, 'Followup with XYZ client', 'Followup with XYZ client', '10:20 AM', 3),
+		new TaskModel(4, 'Pay Electricity bill', 'Pay Electricity bill', '11:00 AM', 1),
+		new TaskModel(5, 'Search the best tag line for app', 'Search the best tag line for app' , '11:10 AM', 1),
+		new TaskModel(6, 'Demonstrate your application to Simon', 'Demonstrate your application to Simon', '12:00 AM', 0),
+		new TaskModel(7, 'Push all application code to git', 'Push all application code to git', '12:10 PM', 2),
+		new TaskModel(8, 'Manage CRUD for tasks', 'Manage CRUD for tasks' , '12:30 PM', 3),
+		new TaskModel(9, 'Prepare awesome template for your task', 'Prepare awesome template for your task', '01:00 PM', 2),
+		new TaskModel(10, 'Work on memory and will power', 'Work on memory and will power', '02:00 PM', 3)
+	]
+};
+
 
