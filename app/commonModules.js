ae6efacb25ae6859f98535ec935f96ec91bb4661
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-01-15 15:35:45 +0530

    colors and icons list for category models added

--- app/commonModules.js
+++ app/commonModules.js
@@ -3,15 +3,24 @@
  * like icons and stylesheet from single place for all modules
  */
 import { commonStyles } from './assets/styles/sharedStyles';
-import { styleConstant, globalTokens,layoutAttrib, priorityEnum} from './assets/content';
+import {
+	categoryColors,
+	globalTokens,
+	iconNames,
+	layoutAttrib,
+	priorityEnum,
+	styleConstant
+	} from './assets/content';
 import ActionButton from 'react-native-action-button';
 import Icon from 'react-native-vector-icons/MaterialIcons';
 
 export {
 	ActionButton,
+	categoryColors,
 	commonStyles,
 	globalTokens,
 	Icon,
+	iconNames,
 	layoutAttrib,
 	priorityEnum,
 	styleConstant
