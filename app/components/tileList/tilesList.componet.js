9ee856d8ab2eaf412f30488a7adc9bee0588aa62
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-03-16 22:18:49 +0530

    new tile list component added with new functionalities and features

---
+++ app/components/tileList/tilesList.componet.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { ActivityIndicator, View, FlatList, StyleSheet, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { _, commonStyles, Icon, ActionButton, styleConstant, categoryColors, iconList, layoutAttrib } from '../../commonModules';
+
+export const TileListComponent = (props) => {
+	return (
+		<View style={[commonStyles.flex, commonStyles.borderAll]}>
+			<FlatList
+				contentContainerStyle={[styles.tilesContainer]}
+				data={props.data}
+				keyExtractor={(item, index) => index}
+				renderItem={props.renderItem}
+				onEndReached={props.onEndReached}
+				onEndReachedThreshold={0.5}
+				numColumns={props.numColumns}
+				horizontal={false}
+				ListFooterComponent={() =>
+					props.loading ? <ActivityIndicator size='large' animating /> : null
+				}
+			>
+			</FlatList>
+		</View>
+	)
+}
+
+const styles = StyleSheet.create({
+	tilesContainer: {
+		padding: 3,
+		justifyContent: 'flex-start'
+	}
+});
