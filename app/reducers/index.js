b17145d07f84a83180c054f90da2e430fa223f7f
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-03-16 22:21:29 +0530

    icon redcer added to root reducer collection

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import categoryReducer from './category.reducer';
+import iconReducer from './icon.reducer';
 
 // Combine all the reducers
 const rootReducer = combineReducers({
 	categoryReducer,
+	iconReducer
 })
 
 export default rootReducer;
