de25e264efddaabd7afe76884b430ec54e3718be
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-03-16 22:16:39 +0530

    extra store removed from root navigator

--- App.js
+++ App.js
@@ -8,13 +8,14 @@ import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import store from './app/store';
 import { RootNavigator } from './app/navigators/RootNavigator';
+//import CategoryComponent from './app/screens/category/category.component';
 
 
 export default class App extends Component<{}> {
   render() {
     return (
 		<Provider store={store}>
-			<RootNavigator store={store} />
+			<RootNavigator />
 		</Provider>
     );
   }
