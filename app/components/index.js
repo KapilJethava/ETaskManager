4a46e40016ac6cb55f376736dc409506c251d7b1
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-03-16 22:19:27 +0530

    Removed unextra component and added new component

--- app/components/index.js
+++ app/components/index.js
@@ -1,10 +1,11 @@
 import CategoryListComponent from './categories/CategoryListComponent';
 import { TaskListComponent } from './taskList/TaskListComponent';
-import { AddCategoryComponent } from './categories/AddCategoryComponent';
-
+import { IconMasterComponent } from './iconsMaster/iconmaster.component';
+import { TileListComponent } from './tileList/tilesList.componet';
 export {
 	CategoryListComponent,
+	IconMasterComponent,
 	TaskListComponent,
-	AddCategoryComponent
+	TileListComponent
 }
 
