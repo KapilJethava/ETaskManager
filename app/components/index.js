8defd57821e65443c2e22a9fd20d478c5284bbbd
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-03-16 22:32:36 +0530

    removed extra noted component iconmaster

--- app/components/index.js
+++ app/components/index.js
@@ -1,10 +1,8 @@
 import CategoryListComponent from './categories/CategoryListComponent';
 import { TaskListComponent } from './taskList/TaskListComponent';
-import { IconMasterComponent } from './iconsMaster/iconmaster.component';
 import { TileListComponent } from './tileList/tilesList.componet';
 export {
 	CategoryListComponent,
-	IconMasterComponent,
 	TaskListComponent,
 	TileListComponent
 }
