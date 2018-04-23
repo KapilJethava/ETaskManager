22ed90102434ab6712c4be2325bd32085a25d81b
Jethava, Kapil <Kapil.Jethava@mastercard.com>
2018-04-23 18:42:52 +0530

    multiple styles managed in components

--- app/components/color-tile/color-tile.component.js
+++ app/components/color-tile/color-tile.component.js
@@ -6,7 +6,7 @@ import { FadeIn } from '../../animations';
 export const ColorTile = (props) => {
 	return (
 		<TouchableOpacity onPress={() => props.onPress() }>
-			<View style={[props.containerStyle, { borderColor: props.color }]}>
+			<View style={[...props.containerStyle, { borderColor: props.color }]}>
 				<View style={[commonStyles.generalBG, commonStyles.stretchToParent]} />
 				<View style={[commonStyles.stretchToParent, commonStyles.catBgOpacity, { backgroundColor: props.color }]}></View>
 				<Icon name={props.iconName} style={[props.iconStyle, { color: props.color }]} />
--- app/components/icon-tile/icon-tile.component.js
+++ app/components/icon-tile/icon-tile.component.js
@@ -5,7 +5,7 @@ import { commonStyles, Icon } from '../../commonModules';
 export const IconTile = (props) => {
 	return (
 		<TouchableOpacity onPress={() => props.onPress() }>
-			<View style={[props.containerStyle, commonStyles.generalBG, { borderColor: props.color }]}>
+			<View style={[...props.containerStyle, commonStyles.generalBG, { borderColor: props.color }]}>
 				<Icon name={props.iconName} style={[props.iconStyle, { color: props.color }]} />
 			</View>
 		</TouchableOpacity>
